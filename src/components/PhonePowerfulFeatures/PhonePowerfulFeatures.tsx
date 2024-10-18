fff287b1f65b5072c7a8b3f44a0981e84fffa107
diegomota2374 <diegomota2374@gmail.com>
2024-10-18 20:13:58 -0300

    correção do componente PhonePowerfulFeatures

--- src/components/PhonePowerfulFeatures/PhonePowerfulFeatures.tsx
+++ src/components/PhonePowerfulFeatures/PhonePowerfulFeatures.tsx
@@ -10,12 +10,12 @@ const PhonePowerfulFeatures = () => {
       <div className="absolute w-[599.56px] h-[350px] -right-[250px] top-[80px] bg-[#FBBF24] rounded-full z-[1] -rotate-45"></div>
 
       {/* Phone rectangle */}
-      <div className="w-[220px] h-[485.43px] bg-[#E0F2FE] border-4 border-white rounded-3xl shadow-lg z-[2] p-6">
+      <div className="w-[220px] h-[485.43px] bg-[#E0F2FE] border-4 border-white rounded-3xl shadow-lg z-[2] p-4">
         <div className="relative">
-          <div className="relative bg-white w-full h-[90px] rounded-lg mt-5 overflow-hidden">
-            <div className="absolute w-[60.78px] h-[60.78px] top-[48.91px] left-[125.63px] bg-[#FBBF24] rounded-full "></div>
-            <div className="absolute bg-white w-[65.08px] h-[65.08px] top-[16px] right-[10px] border-4 border-[#E0F2FE] rounded-full "></div>
-            <div className="absolute bg-[#BAE6FD] w-[110.63px] h-[105.6px] -top-[55px] -left-[55px] -rotate-45"></div>
+          <div className="relative bg-white w-full h-[128.32px] rounded-lg mt-1 overflow-hidden">
+            <div className="absolute w-[60.78px] h-[60.78px] top-[85px] left-[135px] bg-[#FBBF24] rounded-full "></div>
+            <div className="absolute bg-white w-[65.08px] h-[65.08px] top-[56px] right-[10px] border-4 border-[#E0F2FE] rounded-full "></div>
+            <div className="absolute bg-[#BAE6FD] w-[180.63px] h-[105.6px] -top-[30px] -left-[75px] -rotate-45"></div>
           </div>
         </div>
 
@@ -23,7 +23,7 @@ const PhonePowerfulFeatures = () => {
           <div className="flex items-center gap-1">
             <div className="bg-[#BAE6FD] w-[45.6px] h-[10.47px] rounded-lg"></div>
             <div className="bg-[#BAE6FD] w-[69.05px] h-[10.47px] rounded-lg"></div>
-            <FaRegHeart className="text-white ml-6" size={20} />
+            <FaRegHeart className="text-white ml-6" size={25} />
           </div>
           <div className="flex gap-2">
             <div className="bg-white w-[60.34px] h-[7.54px] rounded-lg"></div>
@@ -46,10 +46,10 @@ const PhonePowerfulFeatures = () => {
           <div className="absolute bg-[#BAE6FD] w-[62.54px] h-[62.54px] border-2 border-white rounded-full left-[156px] shadow-2xl"></div>
         </div>
 
-        <div className="flex items-center  justify-between py-5 relative mt-20">
+        <div className="flex items-center  justify-between py-4 relative mt-20">
           <div
             className="absolute bg-white w-[215px] h-[3px]
-          top-0 -left-6"
+          top-0 -left-4"
           ></div>
           <FiActivity size={30} className="text-white" />
           <IoSearchOutline size={30} className="text-white" />
